e75b0852563608f96df502a380edabd055518d5b
cryptoquantplatform <cryptoquantplatform@gmail.com>
2025-10-22 18:02:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const adminAuthMiddleware = require('./middleware/adminAuth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1);
 
 // Security middleware
 app.use(helmet());
